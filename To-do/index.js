d2663edae9832a63511d1f2d52a605989fd69bbf
my name <ysy06053@gmail.com>
2023-06-30 14:43:22 +0900

    Todo진행중

---
+++ To-do/index.js
@@ -0,0 +1,24 @@
+const todoInput = document.querySelector("#todo-input");
+
+const createTodo = () => {
+  const todoList = document.querySelector("#todo-list");
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+};
+
+const inputText = () => {
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createTodo();
+  }
+};
