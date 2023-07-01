90c199bca772de8553dac432be697fdc937ec85d
my name <ysy06053@gmail.com>
2023-07-01 15:40:17 +0900

    TodoList Complete

--- To-do/index.js
+++ To-do/index.js
@@ -1,20 +1,36 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+console.log(savedTodoList);
 
-const createTodo = () => {
-  const todoList = document.querySelector("#todo-list");
+const createTodo = (data) => {
+  let todoContents = todoInput.value;
+  if (data) {
+    todoContents = data.contents;
+  }
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
 
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
+    saveItemsFn();
   });
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFn();
+  });
+  if (data?.complete) {
+    newLi.classList.add("complete");
+  }
 
-  newSpan.textContent = todoInput.value;
+  newSpan.textContent = todoContents;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
   todoInput.value = "";
+
+  saveItemsFn();
 };
 
 const inputText = () => {
@@ -22,3 +38,35 @@ const inputText = () => {
     createTodo();
   }
 };
+
+const deleteTodo = () => {
+  const deleteArr = document.querySelectorAll("li");
+  for (let i = 0; i <= deleteArr.length; i++) {
+    deleteArr[i]?.remove();
+  }
+  saveItemsFn();
+};
+
+const saveItemsFn = () => {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
+
+  if (saveItems.length === 0) {
+    localStorage.removeItem("saved-items");
+  } else {
+    const data = JSON.stringify(saveItems);
+    localStorage.setItem("saved-items", data);
+  }
+};
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
